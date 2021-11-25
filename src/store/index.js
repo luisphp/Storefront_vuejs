eb7aed7cca62f6aa71ea671d04c0cfffbf3a9bd7
Luis <lhurtado@labsxd.com>
2021-11-25 15:23:34 -0400

    Top sell Products modifications

--- src/store/index.js
+++ src/store/index.js
@@ -6,9 +6,11 @@ export default createStore({
   },
   mutations: {
     addItemsTo_hotSaleProducts(state, payload){
-      for(){
+      // Lets show just 4 products in TopSellProduct Div
+      for(let i = 0; i < 4; i++){
+        state.hotSaleProducts.push(payload[i])
       }
-      state.hotSaleProducts = payload 
+      
     },
   },
   actions: {
